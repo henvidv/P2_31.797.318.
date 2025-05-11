82fe89ec030d465b28e59d3f8b8f4deccf90c29a
henvidv <alvareshenry150@gmail.com>
2025-05-10 23:52:23 -0400

    Update app.js

--- src/public/app.js
+++ src/public/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { normalize } from 'path';
 
 const app =express();
-const PORT = normalizePort(process.env.PORT || '3000');
+const PORT = normalizePort(process.env.PORT || '10000');
 
 
 //Middleware para archivos estaticos
